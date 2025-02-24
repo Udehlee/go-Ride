6385dac1397482bba987cd8d672d5c444f2a5e11
udehlee <ritzeepita@gmail.com>
2025-02-24 20:21:10 +0100

    add addDriver, savedmatchedRecord functionality

--- service/service.go
+++ service/service.go
@@ -2,37 +2,50 @@ package service
 
 import (
 	"fmt"
+	"time"
 
+	"github.com/Udehlee/go-Ride/db/db"
 	"github.com/Udehlee/go-Ride/engine"
 	"github.com/Udehlee/go-Ride/models"
 )
 
-// MatchingService handles rider-driver matching
 type Service struct {
-	pq *engine.PriorityQueue
-	wp *engine.WorkerPool
+	store db.Repo
+	pq    engine.Priority
+	wp    engine.WorkerP
 }
 
-func NewService(workers int) *Service {
-	pq := engine.NewPriorityQueue()
-	wp := engine.NewWorkerPool(workers, pq)
-	wp.Start()
-	return &Service{pq: pq, wp: wp}
+func NewService(db db.Repo, pq engine.Priority, wp engine.WorkerP) *Service {
+	return &Service{
+		store: db,
+		pq:    pq,
+		wp:    wp,
+	}
 }
 
 // AddDriver adds a new driver to the system
-func (s *Service) AddDriver(id int, lat, lon float64) {
+func (s *Service) AddDriver(id int, name string, lat, lon float64) error {
 	driver := models.Driver{
-		ID:        id,
-		Latitude:  lat,
-		Longitude: lon,
+		DriverID:   id,
+		DriverName: name,
+		Latitude:   lat,
+		Longitude:  lon,
 	}
 	s.pq.Insert(driver)
+
+	err := s.store.SaveDriver(driver)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
-// RequestRide handles a ride request and finds a driver
-func (s *Service) RequestRide(passengerID int, passengerName string, lat, lon float64) (models.Driver, error) {
+// RequestRide handles a ride request
+// and finds a driver
+// save matched driver and passsenger to db
+func (s *Service) RequestRide(passengerID int, passengerName string, lat, lon float64) (models.MatchedRide, error) {
 	result := make(chan models.Driver)
+
 	req := models.RideRequest{
 		PassengerID:   passengerID,
 		PassengerName: passengerName,
@@ -44,13 +57,29 @@ func (s *Service) RequestRide(passengerID int, passengerName string, lat, lon fl
 	s.wp.Submit(req)
 	matchedDriver := <-result
 
-	if matchedDriver.ID == 0 {
+	if matchedDriver.DriverID == 0 {
 		fmt.Println("No available drivers")
-	} else {
-		fmt.Printf("Rider %d matched with Driver %d\n", passengerID, matchedDriver.ID)
+		return models.MatchedRide{}, fmt.Errorf("no available drivers")
 	}
 
-	return matchedDriver, nil
+	return s.SaveMatchedRideRecord(matchedDriver.DriverID, passengerID)
+}
+
+func (s *Service) SaveMatchedRideRecord(driverID, passengerID int) (models.MatchedRide, error) {
+	matchedRide := models.MatchedRide{
+		DriverID:    driverID,
+		PassengerID: passengerID,
+		RideStatus:  "matched",
+		CreatedAt:   time.Now(),
+	}
+
+	err := s.store.SaveMatchedRides(matchedRide)
+	if err != nil {
+		return models.MatchedRide{}, fmt.Errorf("failed to save matched ride: %w", err)
+	}
+
+	fmt.Printf("Rider %d matched with Driver %d\n", passengerID, driverID)
+	return matchedRide, nil
 }
 
 // Close shuts down the matching service
