6ec8a4313a662b40a4a84446a2713a4b99c32501
udehlee <ritzeepita@gmail.com>
2025-03-01 18:12:38 +0100

    remove unnecessay comment

--- utils/utils.go
+++ utils/utils.go
@@ -2,7 +2,6 @@ package utils
 
 import geo "github.com/kellydunn/golang-geo"
 
-// HaversineDistance calculates the great-circle distance between two points
 // CalculateDistance returns the great-circle distance between two coordinates
 func CalculateDistance(lat1, lon1, lat2, lon2 float64) float64 {
 	p1 := geo.NewPoint(lat1, lon1)
