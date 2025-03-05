2415b48319558b6e9a29add2d3192750accc8740
udehlee <ritzeepita@gmail.com>
2025-03-05 21:44:56 +0100

    change driver_id and passenger_id to int

--- db/migrations/000002_create_matched_rides_table.up.sql
+++ db/migrations/000002_create_matched_rides_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE matched_rides (
     id SERIAL PRIMARY KEY,
-    passenger_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    driver_id UUID REFERENCES users(id) ON DELETE SET NULL,
+    passenger_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    driver_id INT REFERENCES users(id) ON DELETE SET NULL,
     latitude DOUBLE PRECISION NOT NULL,
     longitude DOUBLE PRECISION NOT NULL,
     created_at TIMESTAMP DEFAULT NOW(),
